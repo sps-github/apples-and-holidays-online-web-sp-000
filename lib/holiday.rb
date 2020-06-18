2bd09671010a7f89f34977d018b040274f287955
Sarah Peters <sarahpieters15@gmail.com>
2020-06-18 12:29:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,8 +34,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.map do |season, holiday|
-    binding.pry
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday_name, supplies|
+      binding.pry
+    end
   end
-
 end
